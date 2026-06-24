d76031de0b63835a2429fafa6c55a8dcd14483e3
loorenna <lorenaalisson2004@gmail.com>
2026-06-23 22:36:38 -0500

    Implementación de login.fxml y LoginController

--- src/main/java/org/example/sistemaparticipantes/controller/LoginController.java
+++ src/main/java/org/example/sistemaparticipantes/controller/LoginController.java
@@ -1,4 +1,75 @@
 package org.example.sistemaparticipantes.controller;
 
+
+import org.example.sistemaparticipantes.util.Alertas;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class LoginController {
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    public void ingresar(ActionEvent event) {
+
+        if (txtUsuario.getText().isEmpty() ||
+                txtPassword.getText().isEmpty()) {
+
+            Alertas.advertencia("Complete todos los campos");
+
+            return;
+
+        }
+
+        if (txtUsuario.getText().equals("admin") &&
+                txtPassword.getText().equals("1234")) {
+
+            try {
+
+                FXMLLoader loader = new FXMLLoader(
+                        getClass().getResource("/ec/edu/esfot/crud.fxml"));
+
+                Stage stage = new Stage();
+
+                stage.setScene(new Scene(loader.load()));
+
+                stage.setTitle("CRUD Participantes");
+
+                stage.show();
+
+                Stage login =
+                        (Stage) txtUsuario.getScene().getWindow();
+
+                login.close();
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+        } else {
+
+            Alertas.error("Usuario o contraseña incorrectos");
+
+        }
+
+    }
+
+    @FXML
+    public void salir() {
+
+        System.exit(0);
+
+    }
+
 }
