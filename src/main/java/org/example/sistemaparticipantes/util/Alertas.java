ec51427e4dd061ba6062feadcf601343375a3788
loorenna <lorenaalisson2004@gmail.com>
2026-06-23 22:32:43 -0500

    Implementación de Alertas

--- src/main/java/org/example/sistemaparticipantes/util/Alertas.java
+++ src/main/java/org/example/sistemaparticipantes/util/Alertas.java
@@ -1,4 +1,43 @@
 package org.example.sistemaparticipantes.util;
 
+import javafx.scene.control.Alert;
+
 public class Alertas {
+
+    public static void informacion(String mensaje){
+
+        Alert alert=new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setHeaderText(null);
+
+        alert.setContentText(mensaje);
+
+        alert.showAndWait();
+
+    }
+
+    public static void error(String mensaje){
+
+        Alert alert=new Alert(Alert.AlertType.ERROR);
+
+        alert.setHeaderText(null);
+
+        alert.setContentText(mensaje);
+
+        alert.showAndWait();
+
+    }
+
+    public static void advertencia(String mensaje){
+
+        Alert alert=new Alert(Alert.AlertType.WARNING);
+
+        alert.setHeaderText(null);
+
+        alert.setContentText(mensaje);
+
+        alert.showAndWait();
+
+    }
+
 }
